e12115395a76014417c238ef4705c2b01cfe23a4
gcgloven <gong.chen.ccstudio@gmail.com>
2019-12-21 12:04:10 +0800

    Update delete_bookstore.py

--- delete_bookstore.py
+++ delete_bookstore.py
@@ -22,6 +22,7 @@ while(not next_step):
                     aws_secret_access_key=aws_secret_key,
                     region_name=region_name)
 
+    
     try:
         ec2_functions.list_ec2_instances(ec2)
         next_step = True
@@ -52,13 +53,29 @@ f = open("mysql_groupID","r")
 mysql_groupID  = f.read()
 f.close
 
+f = open("hadoop_node_instances","r")
+hadoop_node_instance  = f.read().split('/n')[:-1]
+f.close
+
+f = open("hadoop_node_groupID","r")
+hadoop_node_groupID  = f.read()
+f.close
+
+
+
 f= open("key_pair","r")
 key_pair  = f.read()
 f.close
 
+f= open("hadoop_key_pair","r")
+hadoop_key_pair  = f.read()
+f.close
+
 print('Terminating all instances...')
 
-ec2_functions.terminate_instances(ec2, [node_instance, mongo_instance, mysql_instance])
+instances = [node_instance, mongo_instance, mysql_instance] + hadoop_node_instance 
+
+ec2_functions.terminate_instances(ec2, instances)
 
 while(ec2_functions.terminate_instances(ec2, [node_instance])['TerminatingInstances'][0]['CurrentState']['Name'] != 'terminated'):
     print('Waiting for instances to terminate...')
@@ -74,6 +91,18 @@ while(ec2_functions.terminate_instances(ec2, [mysql_instance])['TerminatingInsta
     print('Waiting for instances to terminate...')
     sleep(1)
 ec2_functions.delete_security_group(ec2, mysql_groupID)
+
+for hadoop_instance in hadoop_node_instance:
+    print(hadoop_instance)
+
+    while(ec2_functions.terminate_instances(ec2, [hadoop_instance])['TerminatingInstances'][0]['CurrentState']['Name'] != 'terminated'):
+        print('Waiting for instances to terminate...')
+        sleep(1)
+        
+ec2_functions.delete_security_group(ec2, hadoop_node_groupID)
+
+
+
 
 print('Deleting Key-pair...')
 ec2_functions.delete_key_pair(ec2, key_pair)
@@ -105,9 +134,18 @@ if os.path.exists("mongo_groupID"):
 
 if os.path.exists("mysql_groupID"):
   os.remove("mysql_groupID")
+  
+if os.path.exists("hadoop_node_instances"):
+  os.remove("hadoop_node_instances")  
+  
+if os.path.exists("hadoop_node_groupID"):
+  os.remove("hadoop_node_groupID")
 
 if os.path.exists("key_pair"):
   os.remove("key_pair")
+
+if os.path.exists("hadoop_key_pair"):
+  os.remove("hadoop_key_pair")
 
 if os.path.exists(key_pair + '.pem'):
   os.remove(key_pair + '.pem')
